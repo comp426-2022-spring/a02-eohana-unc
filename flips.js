716eaf9d1c09998a5025daa7c3e0ff141c3c8540
Elad Ohana <69722689+eohana-unc@users.noreply.github.com>
2022-02-24 20:40:52 -0500

    Changed output of flips to remove '...' at the end

--- flips.js
+++ flips.js
@@ -3,10 +3,24 @@ import { coinFlips, countFlips } from "./modules/coin.mjs";
 
 const args = minimist(process.argv);
 
+function output_flips(flips) {
+  let result = `[`
+  
+  for (let i = 0; i < flips.length; i++){
+    if (i != 0) {
+      result += (`,`)
+    }
+    result +=(`'${flips[i]}'`)
+  }
+  result += `]`
+  return result;
+}
+
 // console.log(args)
+
 
 let numflips = args['number'] || 1
 const flips = coinFlips(numflips);
-console.log(flips);
+console.log(output_flips(flips));
 console.log(countFlips(flips));
 
